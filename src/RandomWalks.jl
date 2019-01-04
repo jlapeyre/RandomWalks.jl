7d0ed389550466a6af79029399b58bf08c87db85
John Lapeyre <jlapeyre@users.noreply.github.com>
2019-01-04 18:08:26 +0100

    Files generated by PkgTemplates

---
+++ src/RandomWalks.jl
@@ -0,0 +1,5 @@
+module RandomWalks
+
+greet() = print("Hello World!")
+
+end # module
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using RandomWalks
+using Test
+
+@testset "RandomWalks.jl" begin
+    # Write your own tests here.
+end
