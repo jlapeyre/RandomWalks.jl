07202a8f68454d59f3c0de31211c75cf49d55546
John Lapeyre <jlapeyre@users.noreply.github.com>
2019-01-08 14:31:57 +0100

    Test mortal walk code with WalkF

--- README.md
+++ README.md
@@ -5,4 +5,4 @@
 [![Codecov](https://codecov.io/gh/jlapeyre/RandomWalks.jl/branch/master/graph/badge.svg)](https://codecov.io/gh/jlapeyre/RandomWalks.jl)
 [![Coveralls](https://coveralls.io/repos/github/jlapeyre/RandomWalks.jl/badge.svg?branch=master)](https://coveralls.io/github/jlapeyre/RandomWalks.jl?branch=master)
 
-Nothing to see here, ... move along.
+Highly configurable random walk framework.
--- examples/walk.jl
+++ examples/walk.jl
@@ -1,9 +1,5 @@
 using RandomWalks
 using Distributions, BenchmarkTools
-using RandomWalks.LatticeVars
-using RandomWalks.Lattices
-using RandomWalks.Walks: Walk, MortalWalk, WalkPlan, trial!
-using RandomWalks.Actors
 using EmpiricalCDFs
 
 ntrials = 10^3
@@ -14,7 +10,8 @@ lattice_rates = LatticeVar(Exponential(lambda))
 waiting_times = LatticeVarParam(Pareto(0.5))
 quenched_lattice = Lattice(waiting_times, lattice_rates)
 
-walk = MortalWalk()
+walk = WalkF(status = Mortal())
+#walk = MortalWalk()
 latwalk = LatticeWalk(quenched_lattice, walk)
 
 store_range = 1:.3:7.5
@@ -22,7 +19,7 @@ storing_actor = storing_nsteps_actor(logrange(store_range))
 step_limit_actor = StepLimitActor(nsteps)
 theactors = ActorSet(step_limit_actor, storing_actor)
 walk_plan = WalkPlan(latwalk, theactors)
-ea = ECDFActor(storing_actor);
+ea = ECDFsActor(storing_actor);
 
 @time trial!(walk_plan, 1:ntrials, ea);
 
