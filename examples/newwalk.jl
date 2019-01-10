e9b0e9688186ff44ce479abbf4535964af1ac6de
John Lapeyre <jlapeyre@users.noreply.github.com>
2019-01-10 18:04:44 +0100

    check

--- examples/newwalk.jl
+++ examples/newwalk.jl
@@ -13,10 +13,30 @@ using Distributions, BenchmarkTools
 using EmpiricalCDFs
 using DataFrames
 
+struct WalkParams
+    dimension::Int
+    ntrials::Int
+    nsteps::Int
+    lambda::Float64
+    alpha::Float64
+end
+
+function Base.print(wp::WalkParams)
+    println("dimension = ", wp.dimension)
+end
+
 function getcdfs(; dimension = 2, ntrials = 10^3, nsteps = 10^3, lambda = 1e2)
+    alpha = 1/2
+    wparams = WalkParams(
+        dimension,
+        ntrials,
+        nsteps,
+        lambda,
+        alpha
+        )
 
     lattice_rates = LatticeVar{dimension}(Exponential(lambda))
-    waiting_times = LatticeVarParam{dimension}(Pareto(0.5))
+    waiting_times = LatticeVarParam{dimension}(Pareto(alpha))
     quenched_lattice = Lattice(waiting_times, lattice_rates)
 
     walk = MortalWalk(WalkB{dimension}())
@@ -36,7 +56,7 @@ function getcdfs(; dimension = 2, ntrials = 10^3, nsteps = 10^3, lambda = 1e2)
     end
     tcdfs = get_ecdf_times(ecdfs_actor);
     ptcdfs = prune(tcdfs)
-    return (ptcdfs, ntrials, trial_actors)
+    return (ptcdfs, ntrials, trial_actors, wparams)
 end
 
 function cdfrep(ptcdfs, ntrials)
@@ -49,12 +69,13 @@ function cdfrep(ptcdfs, ntrials)
               [:time, :ncounts, :survp, :max_steps, :minmax_sites, :median_nsites, :mean_nsites, :std_nsites])
 end
 
-function vstrials(; lambda = 1e2, dimension = 2)
+function vstrials(; dimension = 2, nsteps = 10^7, lambda = 1e2)
     for nexp in 1:7
         for fac in (1, 3, 6)
-            (tcdfs, ntrials, trial_actors) = getcdfs(;ntrials = fac * 10^nexp, nsteps=10^6, lambda=lambda, dimension = dimension)
+            (tcdfs, ntrials, trial_actors, wparams) = getcdfs(;ntrials = fac * 10^nexp,
+                               nsteps=nsteps, lambda=lambda, dimension = dimension)
             df = cdfrep(tcdfs, ntrials)
-            @info "ntrials = $ntrials"
+            println(wparams)
             println(df)
             println()
         end
